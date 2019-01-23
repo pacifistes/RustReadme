be58708c8df2bf884cdd0a223f2f247a66a5f38e
pacifistes <bbrunell@student.42.fr>
2019-01-23 15:37:52 +0100

    avancement du chapitre jusaqu 'au 10-11

--- readmeRust.rs
+++ readmeRust.rs
@@ -6,7 +6,8 @@ cargo build		// Compile
 cargo check		// Compile but dont get executalbe (more faster than build)
 cargo run		// Compile + Run
 cargo update	// update the version of all the crates (3.14 -> 3.16); If need go 4.0 change in Cargo.toml directly
-
+cargo new adder --lib // ADD to library
+cargo test // run test
 *******************************************************************
 
 use std::io;
@@ -46,4 +47,395 @@ USE VARIABLE WITH SAME NAME AS default keyword
 let r#fn
 r#match();
 
+r#list
 
+
+// let r#let
+
+*******************************************************************
+ assert_eq!(result, 20);
+*******************************************************************
+let user2 = User {
+    email: String::from("another@example.com"),
+    username: String::from("anotherusername567"),
+    active: user1.active,
+    sign_in_count: user1.sign_in_count,
+};
+*******************************************************************
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+let black = Color(0, 0, 0);
+let origin = Point(0, 0, 0);
+
+
+*******************************************************************
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!("rect1 is {:?}", rect1);
+}
+
+*******************************************************************
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+let home = IpAddr::V4(127, 0, 0, 1);
+
+let loopback = IpAddr::V6(String::from("::1"));
+*******************************************************************
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+*******************************************************************
+mod sound {
+    pub mod instrument {
+        pub fn clarinet() {
+            // Function body code goes here
+        }
+    }
+}
+fn main() {
+    // Absolute path
+    crate::sound::instrument::clarinet();
+
+    // Relative path
+    sound::instrument::clarinet();
+}
+
+
+*******************************************************************
+mod plant {
+    pub struct Vegetable {
+        pub name: String,
+        id: i32,
+    }
+
+    impl Vegetable {
+        pub fn new(name: &str) -> Vegetable {
+            Vegetable {
+                name: String::from(name),
+                id: 1,
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut v = plant::Vegetable::new("squash");
+
+    v.name = String::from("butternut squash");
+    println!("{} are delicious", v.name);
+
+    // The next line won't compile if we uncomment it:
+    // println!("The ID is {}", v.id);
+}
+
+
+
+mod sound {
+    pub mod instrument {
+        pub fn clarinet() {
+            // Function body code goes here
+        }
+    }
+}
+
+use crate::sound::instrument::clarinet;
+
+fn main() {
+    clarinet();
+    clarinet();
+    clarinet();
+}
+*******************************************************************
+Vector Chapitre 8
+
+let v = vec![1, 2, 3, 4, 5];
+
+let third: &i32 = &v[2];
+println!("The third element is {}", third);
+
+match v.get(2) {
+    Some(third) => println!("The third element is {}", third),
+    None => println!("There is no third element."),
+}
+*******************************************************************
+String
+
+//Add string
+let mut s = String::from("foo");
+s.push_str("bar");
+//Add characrer
+let mut s = String::from("lo");
+s.push('l');
+
+
+let s1 = String::from("Hello, ");
+let s2 = String::from("world!");
+let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+
+
+let s1 = String::from("tic");
+let s2 = String::from("tac");
+let s3 = String::from("toe");
+
+let s = format!("{}-{}-{}", s1, s2, s3);
+
+
+let len = String::from("Здравствуйте").len(); // return len of number of byte (24)
+
+for c in "नमस्ते".chars() {
+    println!("{}", c);
+}
+
+for b in "नमस्ते".bytes() {
+    println!("{}", b);
+}
+*******************************************************************
+HashMap
+
+use std::collections::HashMap;
+
+let mut scores = HashMap::new();
+
+scores.insert(String::from("Blue"), 10);
+scores.insert(String::from("Yellow"), 50);
+
+---
+use std::collections::HashMap;
+
+let teams  = vec![String::from("Blue"), String::from("Yellow")];
+let initial_scores = vec![10, 50];
+
+let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+---
+use std::collections::HashMap;
+let mut scores = HashMap::new();
+
+scores.insert(String::from("Blue"), 10);
+scores.insert(String::from("Yellow"), 50);
+
+let team_name = String::from("Blue");
+let score = scores.get(&team_name);
+---
+for (key, value) in &scores {
+    println!("{}: {}", key, value);
+}
+---
+use std::collections::HashMap;
+
+let mut scores = HashMap::new();
+
+scores.insert(String::from("Blue"), 10);
+scores.insert(String::from("Blue"), 25);
+
+println!("{:?}", scores);
+---
+Only Inserting a Value If the Key Has No Value
+scores.entry(String::from("Yellow")).or_insert(50);
+---
+use std::collections::HashMap;
+
+let text = "hello world wonderful world";
+
+let mut map = HashMap::new();
+
+for word in text.split_whitespace() {
+    let count = map.entry(word).or_insert(0);
+    *count += 1;
+}
+
+println!("{:?}", map);
+ {"world": 2, "hello": 1, "wonderful": 1}
+
+*******************************************************************
+Chapitre 9 -- Error Handling
+unwrap // CALL PANIC if funcion return Err
+expect // Same as unwrap but let u tell the message of panic
+
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+            },
+            other_error => panic!("There was a problem opening the file: {:?}", other_error),
+        },
+    };
+}
+---
+use std::fs::File;
+use std::io::ErrorKind;
+
+fn main() {
+    let f = File::open("hello.txt").map_err(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Tried to create file but there was a problem: {:?}", error);
+            })
+        } else {
+            panic!("There was a problem opening the file: {:?}", error);
+        }
+    });
+}
+---
+use std::fs::File;
+
+fn main() {
+    let f = File::open("hello.txt").unwrap();
+}
+---
+use std::fs::File;
+
+fn main() {
+    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+}
+---
+
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+--- 9.2
+The ? placed after a Result value is defined to work in almost the same way as the match
+If the function call return Err,we stop this function and send the error
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+---
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+---
+use std::error::Error;
+use std::fs::File;
+
+fn main() -> Result<(), Box<dyn Error>> // Any kind of error {
+    let f = File::open("hello.txt")?;
+
+    Ok(())
+}
+*******************************************************************
+
+CHAPITRE 10.2 TRAIT | INTERFACE
+//
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+
+	// oR DEFAYKT
+	fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+*******************************************************************
+TEST Chapitre 11
+cargo new filename --lib
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
